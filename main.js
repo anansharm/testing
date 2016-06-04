f6b3215a196b38865bf7297b92809b6dd5247ba1
anansharm <anansharm@gmail.com>
2016-06-04 12:34:11 -0700

    trying some jqury variables use and targetting elements with nth-child

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+	//sorting function
 	$('ol').sortable();
+
+	//To target thew second element and change the background color
+	var $li = $('li:nth-child(2)');
+	$li.css({'background-color':'red'});
 });
+
