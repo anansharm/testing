b7a61fefa9a5ada468341e96b240bc0de5a7e8f4
anansharm <anansharm@gmail.com>
2016-06-09 15:14:43 -0700

    Fizz and Buzz game added

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ $(document).ready(function(){
 	$li.css({'background-color':'red'});
 });
 
+//FizzBuzz Exercise or call it a game if you like
+function fizz(i){
+	    	if (i<=0 || i > 1000) {
+	    		return "Enter a number between 1 and 1000";
+	    	}
+	    	
+			if (i%3==0 && i%5==0) {
+				return "FizzBuzz";
+			}
+			else if (i%3==0) {
+				return "Fizz";
+			}
+			else if(i%5==0) {
+				return "Buzz";
+			}
+			else{
+				return "Neither Fizz nor Buzz"
+			}	
+	    }
+
+document.getElementById('UserInputAmount').onkeypress = function(e) {
+    var event = e || window.event;
+    var charCode = event.which || event.keyCode;
+
+    if ( charCode == '13' ) {
+      // Enter pressed
+	    var x = document.getElementById("UserInputAmount").value;
+		document.getElementById("demo").innerHTML = fizz(x);
+	    return false;
+    }
+}
